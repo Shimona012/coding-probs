1a422a3edeeae4b7e758c3b5945fdf9a08a289ac
Shimona <74822423+Shimona012@users.noreply.github.com>
2026-09-18 18:21:20 +0530

    ✅ WATERCOOLER2 - Rating 798 | Easy | Python | Runtime: N/A | Memory: N/A | 2026-09-18

--- codechef/easy/WATERCOOLER2---Rating-798/solution.py
+++ codechef/easy/WATERCOOLER2---Rating-798/solution.py
@@ -2,9 +2,7 @@
 T=int(input())
 for i in range(T):
     X,Y=map(int,input().split())
-    x=int(X)
-    month=0
-    while X<Y:
-        month+=1
-        X+=x
-    print(month)
+    if X>=Y:
+        print(0)
+    else:
+        print((Y-1)//X)
