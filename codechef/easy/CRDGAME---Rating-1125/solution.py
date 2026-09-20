0d2ce9c15b5d153a0ab30ab42e9dbc92eb860ba0
Shimona <74822423+Shimona012@users.noreply.github.com>
2026-09-20 12:08:33 +0530

    ✅ CRDGAME - Rating 1125 | Easy | Python | Runtime: N/A | Memory: N/A | 2026-09-20

---
+++ codechef/easy/CRDGAME---Rating-1125/solution.py
@@ -0,0 +1,20 @@
+# cook your dish here
+T=int(input())
+for i in range(T):
+    N=int(input())
+    p1,p2=0,0
+    for i in range(N):
+        n1,n2=[sum(map(int,x))for x in input().split()]
+        if n1==n2:
+            p1+=1
+            p2+=1
+        elif n1>n2:
+            p1+=1
+        else:
+            p2+=1
+    if p1>p2:
+        print(0,p1)
+    elif p2>p1:
+        print(1,p2)
+    else: 
+        print(2,p1)
