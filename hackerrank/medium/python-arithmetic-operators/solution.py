dd436eb646b0114724bf15288c07dd310c2278f3
Shimona <74822423+Shimona012@users.noreply.github.com>
2026-09-23 07:25:03 +0530

    ✅ Arithmetic Operators | Medium | Python | Runtime: N/A | Memory: N/A | 2026-09-23

--- hackerrank/medium/python-arithmetic-operators/solution.py
+++ hackerrank/medium/python-arithmetic-operators/solution.py
@@ -1,22 +1,4 @@
-#!/bin/python3
-
-import math
-import os
-import random
-import re
-import sys
-
-
-
 if __name__ == '__main__':
-    n = int(input().strip())
-    if n%2==0:
-        if n>1:
-            if n<6:
-                print("Not Weird")
-            elif n<21:
-                print("Weird")
-            else:
-                print("Not Weird")
-    else:
-        print("Weird")
+    a = int(input())
+    b = int(input())
+    print(a+b,a-b,a*b,sep='\n')
