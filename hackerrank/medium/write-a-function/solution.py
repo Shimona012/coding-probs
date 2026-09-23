327f419a7b031d5888e8bb544d549efff0e2c4ef
Shimona <74822423+Shimona012@users.noreply.github.com>
2026-09-23 07:29:06 +0530

    ✅ Write a function | Medium | Python | Runtime: N/A | Memory: N/A | 2026-09-23

--- hackerrank/medium/write-a-function/solution.py
+++ hackerrank/medium/write-a-function/solution.py
@@ -1,4 +1,13 @@
-if __name__ == '__main__':
-    n = int(input())
-    for i in range(n):
-        print(i**2)
+def is_leap(year):
+    leap = False
+    if year%4==0:
+        if year%100==0:
+            if year%400==0:
+                leap=True
+        else:
+            leap=True
+    # Write your logic here
+    
+    
+    return leap
+
