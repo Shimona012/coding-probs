fa54bfab26d5b85a336a6c7d3c802d4df11cc396
Shimona <74822423+Shimona012@users.noreply.github.com>
2026-09-23 07:30:57 +0530

    ✅ Print Function | Medium | Python | Runtime: N/A | Memory: N/A | 2026-09-23

--- hackerrank/medium/python-print/solution.py
+++ hackerrank/medium/python-print/solution.py
@@ -1,13 +1,6 @@
-def is_leap(year):
-    leap = False
-    if year%4==0:
-        if year%100==0:
-            if year%400==0:
-                leap=True
-        else:
-            leap=True
-    # Write your logic here
-    
-    
-    return leap
-
+if __name__ == '__main__':
+    n = int(input())
+    nums=str()
+    for i in range(1,n+1):
+        nums+=str(i)
+    print(nums)
